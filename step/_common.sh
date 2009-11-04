f29232aa5702f018daa86fb62fbb6127d71dc372
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-11-04 21:10:30 +0000

    Fixing mistake
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1111 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- step/_common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define function to log messages with timestamps
-function log {
-    echo "`date -u +"%Y-%m-%d %H:%M:%S %Z"`		${1}"
-}
-
-# Figure out where this script is located 
-source `cd ${0%/*} && pwd`/_variables
