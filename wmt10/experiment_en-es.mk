442ce258106fe941ce597a914e0665ed2001e53c
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-02-22 04:33:12 +0000

    Added lm targets for all pairs
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1463 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- wmt10/experiment_en-es.mk
+++ wmt10/experiment_en-es.mk
@@ -17,7 +17,8 @@ export SUBSAMPLER_MANIFEST=news-commentary10.es-en europarl-v5.es-en undoc.2000.
 # Define dev and devtest files to translate
 export FILES_TO_TRANSLATE=newssyscomb2009-src.${SRC} news-test2008-src.${SRC}
 
-
+# Define monolingual target language files to use for training language model
+export LM_TRAINING_FILE_NAMES=europarl-v5.${TGT} news-commentary10.${TGT} news.${TGT}.shuffled undoc.2000.en-es.${TGT}
 
 # Calculate the full path to this make file
 PATH.TO.THIS.MAKEFILE:=$(realpath $(dir $(lastword ${MAKEFILE_LIST})))
