f6c8ba83a56698a6a24ed49fab50222e0b30cfb7
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-02-22 21:05:13 +0000

    Fixed problem
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1468 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- wmt10/experiment_en-es.mk
+++ wmt10/experiment_en-es.mk
@@ -22,11 +22,11 @@ export FILES_TO_TRANSLATE=newssyscomb2009-src.${SRC} news-test2008-src.${SRC}
 # Define monolingual target language files to use for training language model
 export LM_TRAINING_FILE_NAMES=europarl-v5.${TGT} news-commentary10.${TGT} news.${TGT}.shuffled undoc.2000.en-es.${TGT}
 
-# Define files and settings for running MERT
-include ${PATH.TO.THIS.MAKEFILE}/wmt10-mert-config.mk
-
 # Calculate the full path to this make file
 PATH.TO.THIS.MAKEFILE:=$(realpath $(dir $(lastword ${MAKEFILE_LIST})))
+
+# Define files and settings for running MERT
+include ${PATH.TO.THIS.MAKEFILE}/wmt10-mert-config.mk
 
 # Include makefile for configuring the rest of the experiment
 include ${PATH.TO.THIS.MAKEFILE}/jhu-a-node-config.mk
