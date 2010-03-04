0f9f888d14e863e584dfabb408c28c53b5e40c06
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-03-04 20:10:33 +0000

    Added script to extract rules using Hiero
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1511 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10/extract-grammar-hiero.mk
@@ -0,0 +1,53 @@
+# Calculate the full path to this make file
+THIS.MAKEFILE:= $(realpath $(lastword ${MAKEFILE_LIST}))
+
+# Define how to run this file
+define USAGE
+	$(info  )
+	$(info Usage:	make -f ${THIS.MAKEFILE} SRC=srcLang TGT=tgtLang EXTRACT_RULES_JVM_FLAGS=flags SUBSAMPLED_DATA=/path/to/dir BERKELEY_ALIGN_DIR=/path/to/dir JOSHUA=/path/to/joshua EXTRACT_RULES_DIR=/path/to/dir extract-rules)
+	$(info  )
+	$(error )
+endef
+
+
+# If any of these required parameters are not defined,
+#    print usage, then exit
+HIERO_DIR ?= $(call USAGE)
+SRC ?= $(call USAGE)
+TGT ?= $(call USAGE)
+SUBSAMPLED_DATA ?= $(call USAGE)
+BERKELEY_ALIGN_DIR ?= $(call USAGE)
+EXTRACT_RULES_DIR ?= $(call USAGE)
+
+
+
+EXTRACTED_GRAMMAR:=${EXTRACT_RULES_DIR}/${SRC}-${TGT}.grammar
+
+# Convenient target
+extract-grammar: ${EXTRACTED_GRAMMAR}
+all: extract-grammar
+
+
+
+SUFFIX_ARRAY := ${EXTRACT_RULES_DIR}/sa_bin
+
+# Extract grammar
+${EXTRACTED_GRAMMAR}: ${EXTRACT_RULES_DIR}/${SRC}-${TGT}.ini ${SUBSAMPLED_DATA}/combined.test.${SRC} | ${EXTRACT_RULES_DIR}
+	time cat ${SUBSAMPLED_DATA}/combined.test.${SRC} | ${HIERO_DIR}/hiero/decoder.py -c $< -x $@
+
+# Compile corpus
+${SUFFIX_ARRAY} ${EXTRACT_RULES_DIR}/${SRC}-${TGT}.ini: ${BERKELEY_ALIGN_DIR}/alignments/training.${SRC} ${BERKELEY_ALIGN_DIR}/alignments/training.${TGT}  ${BERKELEY_ALIGN_DIR}/alignments/training.align ${SUBSAMPLED_DATA}/combined.test.${SRC} | ${EXTRACT_RULES_DIR}
+	time ${HIERO_DIR}/sa-utils/compile_files.pl \
+		${BERKELEY_ALIGN_DIR}/alignments/training.${SRC} \
+		${BERKELEY_ALIGN_DIR}/alignments/training.${TGT} \
+		${BERKELEY_ALIGN_DIR}/alignments/training.align  \
+		/dev/null \
+		${HIERO_DIR}/sa-utils/decoder.extract.ini \
+		${SUFFIX_ARRAY} > ${EXTRACT_RULES_DIR}/${SRC}-${TGT}.ini
+
+
+# Create directory
+${EXTRACT_RULES_DIR}:
+	mkdir -p $@
+
+
