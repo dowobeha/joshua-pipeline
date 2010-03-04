24ca476e80891fa5da956433078da2ccf931cf51
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-03-04 20:21:40 +0000

    Added config files
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1512 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10-config/012.ExtractGrammarHiero.en-fr.run2.mk
@@ -0,0 +1,59 @@
+################################################################################
+####            Define the purpose of this experimental step:               ####
+####                                                                        ####
+#### This step extracts a grammar using Hiero's rule extractor
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                 Gather information about this make file                ####
+####                                                                        ####
+#### Get the full path to this make file
+PATH.TO.THIS.MAKEFILE:=$(abspath $(dir $(lastword ${MAKEFILE_LIST})))
+####
+#### Get the filename of this make file
+THIS.MAKEFILE.NAME:=$(basename $(notdir $(lastword ${MAKEFILE_LIST})))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Define where the experiment shall be run:               ####
+####                                                                        ####
+include ${PATH.TO.THIS.MAKEFILE}/000.experiment.mk
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                    Define any required variables:                      ####
+####                                                                        ####
+export EXTRACT_RULES_DIR:=${EXPERIMENT_DIR}/${THIS.MAKEFILE.NAME}
+export HIERO_DIR:=/home/zli/work/hiero/copy_2008/sa_copy/sa-hiero_adapted
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Import any immediate prerequisite steps:                ####
+####                                                                        ####
+$(eval $(call import,${PATH.TO.THIS.MAKEFILE}/011.BerkeleyAlign.en-fr.run2.mk))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define the target to be run:                       ####
+####                                                                        ####
+export .DEFAULT_GOAL=extract-grammar
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define how to run this step:                       ####
+####                                                                        ####
+$(eval $(call import,${EXPERIMENT_MAKE_DIR}/extract-grammar-hiero.mk))
+####                                                                        ####
+################################################################################
