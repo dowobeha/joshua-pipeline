db52d569c36a79ef6cf859770afd963a2426d801
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-03-05 22:08:48 +0000

    Added files
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1525 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10-config/023.SubsampleRecasing.fr.mk
@@ -0,0 +1,65 @@
+################################################################################
+####            Define the purpose of this experimental step:               ####
+####                                                                        ####
+#### This step defines the test data to be used during subsampling
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                 Gather information about this make file                ####
+####                                                                        ####
+#### Get the full path to this make file
+PATH.TO.THIS.MAKEFILE:=$(abspath $(dir $(lastword ${MAKEFILE_LIST})))
+####
+#### Get the filename of this make file
+THIS.MAKEFILE.NAME:=$(basename $(notdir $(lastword ${MAKEFILE_LIST})))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Define where the experiment shall be run:               ####
+####                                                                        ####
+include ${PATH.TO.THIS.MAKEFILE}/000.experiment.mk
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                    Define any required variables:                      ####
+####                                                                        ####
+export SUBSAMPLED_DATA:=${EXPERIMENT_DIR}/${THIS.MAKEFILE.NAME}
+export SRC:=lowercase
+export TGT:=truecase
+export NORMALIZED_DATA:=${EXPERIMENT_DIR}/020.RecasingCorpus.fr
+export FILES_TO_TRANSLATE:=newstest2010.fr.lowercase
+export SUBSAMPLER_MANIFEST:=news-commentary10.fr-en.fr europarl-v5.fr-en.fr giga-fren.release2.fr undoc.2000.en-fr.fr
+export FILTER_SCRIPT:=${EXPERIMENT_MAKE_DIR}/filter-sentences.pl
+export SUBSAMPLER_JVM_FLAGS:=-Xms30g -Xmx30g -Dfile.encoding=utf8
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Import any immediate prerequisite steps:                ####
+####                                                                        ####
+$(eval $(call import,${PATH.TO.THIS.MAKEFILE}/003.Joshua.mk))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define the target to be run:                       ####
+####                                                                        ####
+export .DEFAULT_GOAL=subsample
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define how to run this step:                       ####
+####                                                                        ####
+$(eval $(call import,${EXPERIMENT_MAKE_DIR}/subsample.mk))
+####                                                                        ####
+################################################################################
