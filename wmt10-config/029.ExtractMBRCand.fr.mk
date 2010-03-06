c8e0881c0b14316ab111c53e4a9695a63ebec442
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-03-06 20:20:31 +0000

    Added configs
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1530 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10-config/029.ExtractMBRCand.fr.mk
@@ -0,0 +1,59 @@
+################################################################################
+####            Define the purpose of this experimental step:               ####
+####                                                                        ####
+#### This step extracts the 1-best mbr candidate translations
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                 Gather information about this make file                ####
+####                                                                        ####
+#### Get the full path to this make file
+PATH.TO.THIS.MAKEFILE:=$(abspath $(dir $(lastword ${MAKEFILE_LIST})))
+####
+#### Get the filename of this make file
+THIS.MAKEFILE.NAME:=$(basename $(notdir $(lastword ${MAKEFILE_LIST})))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Define where the experiment shall be run:               ####
+####                                                                        ####
+include ${PATH.TO.THIS.MAKEFILE}/000.experiment.mk
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                    Define any required variables:                      ####
+####                                                                        ####
+JOSHUA_EXTRACT_MBR_CAND_DIR:=${EXPERIMENT_DIR}/${THIS.MAKEFILE.NAME}
+JOSHUA_EXTRACT_MBR_THREADS:=10
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Import any immediate prerequisite steps:                ####
+####                                                                        ####
+$(eval $(call import,${PATH.TO.THIS.MAKEFILE}/27.Recase.fr.mk))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define the target to be run:                       ####
+####                                                                        ####
+export .DEFAULT_GOAL=joshua-extract-mbr-cand
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define how to run this step:                       ####
+####                                                                        ####
+$(eval $(call import,${EXPERIMENT_MAKE_DIR}/extract-mbr-cand.mk))
+####                                                                        ####
+################################################################################
