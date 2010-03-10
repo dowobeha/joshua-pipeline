530714d5eb43605cdf1716ad73bb3ad6b113c867
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-03-10 15:15:17 +0000

    Adding configs to translate newstest2009 as devtest set
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1535 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10-config/015.Translate.de-en.run2.mk
@@ -0,0 +1,62 @@
+################################################################################
+####            Define the purpose of this experimental step:               ####
+####                                                                        ####
+#### This step translates using Joshua
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                 Gather information about this make file                ####
+####                                                                        ####
+#### Get the full path to this make file
+PATH.TO.THIS.MAKEFILE:=$(abspath $(dir $(lastword ${MAKEFILE_LIST})))
+####
+#### Get the filename of this make file
+THIS.MAKEFILE.NAME:=$(basename $(notdir $(lastword ${MAKEFILE_LIST})))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Define where the experiment shall be run:               ####
+####                                                                        ####
+include ${PATH.TO.THIS.MAKEFILE}/000.experiment.mk
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                    Define any required variables:                      ####
+####                                                                        ####
+export JOSHUA_TRANSLATION_DIR:=${EXPERIMENT_DIR}/${THIS.MAKEFILE.NAME}
+export JOSHUA_RULES_DIR:=${EXPERIMENT_DIR}/012.ExtractGrammar.de-en.run2
+export JOSHUA_TRANSLATION_INPUT_DIR:=${EXPERIMENT_DIR}/008.NormalizedData
+export JOSHUA_TRANSLATION_INPUT:=newstest2009-src.de
+export JOSHUA_TRANSLATION_OUTPUT:=newstest2009.en
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Import any immediate prerequisite steps:                ####
+####                                                                        ####
+$(eval $(call import,${PATH.TO.THIS.MAKEFILE}/014.MERT.de-en.bleu.run1.mk))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define the target to be run:                       ####
+####                                                                        ####
+export .DEFAULT_GOAL=joshua_decode
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define how to run this step:                       ####
+####                                                                        ####
+$(eval $(call import,${EXPERIMENT_MAKE_DIR}/decode.mk))
+####                                                                        ####
+################################################################################
