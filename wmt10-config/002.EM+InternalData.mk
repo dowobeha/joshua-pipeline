d6b772b3ad71e74df8040a50fd51d145cfbedeec
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-04-05 15:21:48 +0000

    Added config file
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1555 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10-config/002.EM+InternalData.mk
@@ -0,0 +1,12 @@
+################################################################################
+####            Define the purpose of this experimental step:               ####
+####                                                                        ####
+#### This step decompresses Euromatrix+ internal evaluation data. 
+####                                                                        ####
+################################################################################
+
+$(error This data is not available from a public download site. \
+	This step must be performed manually, \
+	by creating the directory 002.EM+InternalData, \
+	and copying the following files there:	\
+	czech_new_UTF-8.txt  english_new_UTF-8.txt  french_new_UTF-8.txt  german_new_UTF-8.txt)
