e1bee130441ffea3ffda071e38cc2a9826f35a4b
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-04-05 15:44:42 +0000

    Adding more config files for EM+ internal
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/pipeline@1557 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ wmt10-config/012.ExtractGrammar.fr-en.run3.mk
@@ -0,0 +1,59 @@
+################################################################################
+####            Define the purpose of this experimental step:               ####
+####                                                                        ####
+#### This step extracts a grammar
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                 Gather information about this make file                ####
+####                                                                        ####
+#### Get the full path to this make file
+PATH.TO.THIS.MAKEFILE:=$(abspath $(dir $(lastword ${MAKEFILE_LIST})))
+####
+#### Get the filename of this make file
+THIS.MAKEFILE.NAME:=$(basename $(notdir $(lastword ${MAKEFILE_LIST})))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Define where the experiment shall be run:               ####
+####                                                                        ####
+include ${PATH.TO.THIS.MAKEFILE}/000.experiment.mk
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                    Define any required variables:                      ####
+####                                                                        ####
+export EXTRACT_RULES_DIR:=${EXPERIMENT_DIR}/${THIS.MAKEFILE.NAME}
+export EXTRACT_RULES_JVM_FLAGS:=-Xms30g -Xmx30g -Dfile.encoding=utf8
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                Import any immediate prerequisite steps:                ####
+####                                                                        ####
+$(eval $(call import,${PATH.TO.THIS.MAKEFILE}/011.BerkeleyAlign.fr-en.run3.mk))
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define the target to be run:                       ####
+####                                                                        ####
+export .DEFAULT_GOAL=extract-grammar
+####                                                                        ####
+################################################################################
+
+
+################################################################################
+####                     Define how to run this step:                       ####
+####                                                                        ####
+$(eval $(call import,${EXPERIMENT_MAKE_DIR}/extract-grammar.mk))
+####                                                                        ####
+################################################################################
